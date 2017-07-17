bbaf414600a362335f5ec9c644a9174e208000b6
Marc Llopart Riera <larsnow@gmail.com>
2017-07-17 15:02:33 +0200

    playing with views

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,9 +1,9 @@
 
-from .views import tweet_detail_view, tweet_list_view
+from .views import TweetDetailView,TweetListView
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$',tweet_list_view, name='list'),
-    url(r'^1/$',tweet_detail_view, name='home'),
+    url(r'^$',TweetListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$',TweetDetailView.as_view(), name='home'),
 ]
 
--- tweets/views.py
+++ tweets/views.py
@@ -1,23 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView
 from .models import Tweet
 
 
-def tweet_detail_view(request, id=1):
-
-    obj =Tweet.objects.get(id=id)
-
-    context = {
-        "object": obj
-    }
-    return render(request, "tweets/detail_view.html", context)
-
-def tweet_list_view(request):
-
+class TweetDetailView(DetailView):
+    template_name = "tweets/detail_view.html"
     queryset =Tweet.objects.all()
 
-    context = {
-        "object_list": queryset
-    }
+    # def get_object(self):
+    #     print(self.kwargs)
+    #     pk = self.kwargs.get("pk")
+    #     obj = get_object_or_404(Tweet, pk=pk)
+    #     return obj
 
-    return render(request, "tweets/list_view.html", context)
+class TweetListView(ListView):
+    queryset =Tweet.objects.all()
+    template_name = "tweets/list_view.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TweetListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+
+# def tweet_detail_view(request, id=1):
+
+#     obj =Tweet.objects.get(id=id)
+
+#     context = {
+#         "object": obj
+#     }
+#     return render(request, "tweets/detail_view.html", context)
+
+# def tweet_list_view(request):
+
+#     queryset =Tweet.objects.all()
+
+#     context = {
+#         "object_list": queryset
+#     }
+
+#     return render(request, "tweets/list_view.html", context)
