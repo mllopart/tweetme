5f6efc8da0dd94272499c5122630d261f1c67ef8
Marc Llopart Riera <larsnow@gmail.com>
2017-07-31 18:02:50 +0200

    Added some bootstrap design and more tests

--- tweetme/settings/production.py
+++ tweetme/settings/production.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+
+    'tweets',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+
--- tweets/tests.py
+++ tweets/tests.py
@@ -1,11 +1,15 @@
 from django.core.urlresolvers import resolve
+from django.contrib.auth import get_user_model
 from django.test import TestCase, RequestFactory
 from django.http import HttpRequest
 from django.urls import reverse
 
 from .views import TweetListView
+from .models import Tweet
 
 
+
+User = get_user_model()
 
 def setup_view(view, request, *args, **kwargs):
     """Mimic ``as_view()``, but returns view instance.
@@ -24,13 +28,40 @@ class TweetListViewTest(TestCase):
         self.factory = RequestFactory()
 
     def test_get_page_exists(self):
-        request = self.factory.get(reverse('list'))
+        request = self.factory.get(reverse('tweet:list'))
         response = TweetListView.as_view()(request)
-        # if hasattr(response, 'rendered_content'):
-        #     response_content = response.rendered_content
-        # else:
-        #     response_content = response.content
-
         self.assertEqual(response.status_code, 200)
+
+
+class TweetModelTestCase(TestCase):
+
+    def setUp(self):
+        # Tweet.objects.create(
+        #         user= User.objects.first(),
+        #         content='some random content'
+        #     )
+
+        User.objects.create(username="UnitTestUser1")
+
+    def test_tweet_item(self):
+        obj = Tweet.objects.create(
+                user= User.objects.first(),
+                content='some random content'
+            )
+
+        self.assertTrue(obj.content == "some random content")
+        self.assertTrue(obj.id == 1)
+
+        absolute_url = reverse("tweet:retrieve", kwargs={"pk": obj.pk})
+        self.assertEqual(obj .get_absolute_url(), absolute_url)
+
+    def test_tweet_item_url(self):
+        obj = Tweet.objects.create(
+                user= User.objects.first(),
+                content='some random content'
+            )
+
+        absolute_url = reverse("tweet:retrieve", kwargs={"pk": obj.pk})
+        self.assertEqual(obj .get_absolute_url(), absolute_url)
 
 
